577a9f8c48a8c978183a04d8f043a79f084b790d
iliailmer <i.ilmer@icloud.com>
2019-04-10 15:04:40 -0400

    resnet

--- main.py
+++ main.py
@@ -1,14 +1,76 @@
 # from .model.resnet34 import ResNet34
-import torchvision.transforms as transforms
-import torchvision
+import os, sys
+import warnings
+import numpy as np
+import gc
+import pandas as pd
 import torch
 import torch.backends.cudnn as cudnn
-from torch import optim
-from torch import nn
 import torch.nn.functional as F
-import os
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from sklearn.preprocessing import LabelEncoder
+from torch import nn
 from tqdm import tqdm
 
+sys.path.insert(0, os.path.abspath(".."))
+from loader import LoaderSmall
+
+warnings.filterwarnings('ignore')
+np.random.seed(42)
+torch.random.manual_seed(42)
+
+tfms = None  # Compose(transforms=[RandomRotate90(p=0.4), Rotate(p=0.5)])
+
+lesion_type_dict = {
+    'nv': 'Melanocytic nevi',
+    'mel': 'Melanoma',
+    'bkl': 'Benign keratosis-like lesions ',
+    'bcc': 'Basal cell carcinoma',
+    'akiec': 'Actinic keratoses',
+    'vasc': 'Vascular lesions',
+    'df': 'Dermatofibroma'
+}
+
+metadata = pd.read_csv('metadata/HAM10000_metadata.csv')
+
+enc = LabelEncoder()
+metadata['dx'] = enc.fit_transform(metadata['dx'])
+metadata['dx_type'] = enc.fit_transform(metadata['dx_type'])
+metadata['sex'] = enc.fit_transform(metadata['sex'])
+metadata['localization'] = enc.fit_transform(metadata['localization'])
+metadata['lesion_id'] = enc.fit_transform(metadata['lesion_id'])
+labels = metadata.dx.values
+
+imageid_path_dict = {x: f'HAM10000_small/{x}.jpg' for x in metadata.image_id}
+print("Loading data...\n")
+
+trainset = LoaderSmall(imageid_path_dict, labels, train=True, transform=tfms, color_space=None)
+# LoaderSmall(imageid_path_dict, labels, train=True, transform=tfms, color_space=None)
+# #CIFAR10('./', download=True, train=True, transform=ToTensor())#
+testset = LoaderSmall(imageid_path_dict, labels, train=False, transform=tfms, color_space=None)
+# LoaderSmall(imageid_path_dict, labels, train=False, transform=tfms, color_space=None)
+# CIFAR10('./', train=False, transform=ToTensor())#
+
+train_sampler = torch.utils \
+    .data.WeightedRandomSampler(trainset.weights[trainset.train_labels],
+                                len(trainset.weights[trainset.train_labels]),
+                                True)
+test_sampler = torch.utils \
+    .data.WeightedRandomSampler(testset.weights[testset.test_labels],
+                                len(testset.weights[testset.test_labels]),
+                                True)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
+                                          sampler=train_sampler,
+                                          shuffle=False,
+                                          num_workers=0)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64,
+                                         # sampler=test_sampler,
+                                         shuffle=False,
+                                         num_workers=0)
+gc.collect()
 
 class BasicBlock(nn.Module):
     def __init__(self, in_channels,
@@ -62,9 +124,9 @@ class ResNet34(nn.Module):
         self.in_channels = 64
         self.conv1 = nn.Conv2d(in_channels=3,
                                out_channels=64,
-                               kernel_size=3,
-                               stride=1,
-                               padding=1)
+                               kernel_size=7,
+                               stride=2,
+                               padding=3)
         self.bn1 = nn.BatchNorm2d(64)
         self.block1 = self.create_res_block(64, 1, 3)
         self.block2 = self.create_res_block(128, 2, 4)
@@ -96,9 +158,9 @@ class ResNet34(nn.Module):
         return x
 
 
-net = ResNet34()
+net = ResNet34(classes=7)
 transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
+    transforms.RandomCrop(64, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465),
@@ -110,13 +172,13 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='.', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+# trainset = torchvision.datasets.CIFAR10(root='.', train=True, download=True, transform=transform_train)
+# trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='.', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+# testset = torchvision.datasets.CIFAR10(root='.', train=False, download=True, transform=transform_test)
+# testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+# classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 net.cuda()
 if torch.cuda.is_available():
@@ -126,7 +188,7 @@ if torch.cuda.is_available():
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(),
-                      lr=0.1,
+                      lr=0.01,
                       momentum=0.9,
                       weight_decay=5e-4)
 best_acc = 0
